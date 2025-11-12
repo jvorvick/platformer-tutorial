e621f652fd9b0f1c4fd63e9313a0dec8acefd4a5
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-11-12 15:47:15 -0800

    init + platformer game -- exercise: finish

---
+++ code/main.py
@@ -0,0 +1,54 @@
+from settings import * 
+from sprites import *
+from groups import AllSprites
+from player import Player
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Platformer')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups 
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pygame.sprite.Group()
+
+        # setup
+        self.setup()
+
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in map.get_layer_by_name('Main').tiles():
+            CollisionSprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.collision_sprites, self.all_sprites))
+
+        for x, y, image in map.get_layer_by_name('Decoration').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(FRAMERATE) / 1000 
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False 
+            
+            # update
+            self.all_sprites.update(dt)
+
+            # draw 
+            self.display_surface.fill(BG_COLOR)
+            self.all_sprites.draw(self.player.rect.center)
+            pygame.display.update()
+        
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run() 
---
+++ code/player.py
@@ -0,0 +1,37 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, collision_sprites):
+        super().__init__(groups)
+        self.image = pygame.Surface((30,50))
+        self.rect = self.image.get_frect(center = pos)
+
+        self.direction = pygame.Vector2()
+        self.speed = 500
+        self.collision_sprites = collision_sprites
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+
+    def move(self, dt):
+        self.rect.centerx += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.centery += self.direction.y * self.speed * dt
+        self.collision('vertical')
+        
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.rect.left = sprite.rect.right
+                if direction == 'vertical':
+                    if self.direction.y > 0: self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
